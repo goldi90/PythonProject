91535d738fc9f624f70d7a5e5e5bd04661f48db9
goldi90 <47655460+goldi90@users.noreply.github.com>
2021-07-27 15:35:43 +0530

    Status.txt
    Error In Status.txt

--- contactbook/contactbook.py
+++ contactbook/contactbook.py
@@ -17,8 +17,8 @@ def ShowAll():
 
 def CreateContact():
     print(css.color.BOLD+"Note: You are creating Contact"+ css.color.END)
-    personName= input()
-    Number=input()
+    personName= input("Enter The Name Of Person : ")
+    Number=input("Enter The  number of Person : ")
     CreateContacts = mydb.cursor()
     sql = "INSERT INTO contactno.contacts(NameOfPerson, ContactNo) VALUES (%s, %s)"
     val = ("{}".format(personName), "{}".format(Number))
@@ -34,16 +34,46 @@ def UpdateContactNumber():
     val = ("{}".format(Value), "{}".format(Where))
     UpdateContacts.execute(sql,val)
     mydb.commit()
+    mydb.close()
+def UpdateContactName():
+    Where=input("Enter the Number of contact You want to update : ")
+    Value=input("value that You want to replace : ")
+    UpdateContacts=mydb.cursor()
+    sql = "update contactno.contacts set NameOfPerson=(%s) where ContactNO=(%s)"
+    val = ("{}".format(Value), "{}".format(Where))
+    UpdateContacts.execute(sql,val)
+    mydb.commit()
+    mydb.close()
+    
+def DeleteRecordByNumber():
+      Name= input("Enter The Number of Person YOu Want To delete")
+      DeleteContacts=mydb.cursor()
+      sql="delete from contactno.contacts  where ContactNO=(%s);"
+      val=("{}".format(Name))
+      DeleteContacts.execute(sql,val)
+      mydb.commit()
+      mydb.close()
+
+def DeleteRecord():
+      Name= input("Enter The Name of Person YOu Want To delete")
+      DeleteContacts=mydb.cursor()
+      sql="delete from contactno.contacts where NameOfPerson=(%s)"
+      val=("{}".format(Name))
+      DeleteContacts.execute(sql,val)
+      mydb.commit()
+      mydb.close()
 
 
-
-
-Action=input('\033[94m'+"Press C for Creating contact:\nPress A for get all Contact :\nPress UN To update Number   :  "+css.color.END)
+Action=input('\033[94m'+'\033[1m'+"Press C for Creating contact:\nPress A for get all Contact :\nPress UN To update Number   :  \nPress UP To update Name Of person   : "+css.color.END +'\033[91m'+'\033[1m'+"\nPress D To delete Contact   :  \nPress DN To delete Contact By Number   :  "+css.color.END )
 if Action=="c" or Action=="C":
     CreateContact()
 if Action=="a" or Action=="A":
     ShowAll()
 if Action=="un" or Action=="UN":
     UpdateContactNumber()
-    
-
+if Action =="UP" or Action=="up":
+     UpdateContactName()   
+if Action =="d" or Action=="D":
+    DeleteRecord()
+if Action =="DN" or Action =="dn":
+    DeleteRecordByNumber()
