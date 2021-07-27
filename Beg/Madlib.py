da422e7359672415f146b3648a108497e32afbbe
Kaushik <kaushikdwivedi22@gmail.com>
2021-07-28 00:55:13 +0530

    No Other file

--- Beg/Madlib.py
+++
@@ -1,9 +0,0 @@
-# Mad Libs Generator
-Name=input("Name: ")
-Lastname=input("Last Name: ")
-MiddelName=input("Middel Name: ")
-CityName= input("Name Of City Recidence: ")
-Edu=input("Enter The Gradution / Master Course: ")
-print("My Name is"+" "+ Name+" "+Lastname+" "+" "+MiddelName)
-print("I Reside in"+""+CityName)
-print("I have Completed My"+""+Edu)
