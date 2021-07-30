42afa6fe29a5bf78f360e100933eb6abd990d4a6
Kaushik <kaushikdwivedi22@gmail.com>
2021-07-30 18:47:12 +0530

    Random

--- contactbook/contactbook.py
+++ contactbook/contactbook.py
@@ -1,8 +1,7 @@
 from os import name
 import css
 import Function
-
-
+# Nedd To Update
 Action=input('\033[94m'+'\033[1m'+"Press C for Creating contact:\nPress A for get all Contact :\nPress UN To update Number   :  \nPress UP To update Name Of person   : "+css.color.END +'\033[91m'+'\033[1m'+"\nPress D To delete Contact   :  \nPress DN To delete Contact By Number   :  "+css.color.END )
 if Action=="c" or Action=="C":
     Function.CreateContact()
